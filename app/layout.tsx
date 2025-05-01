dc7c397fdb475bc4ba117aaef34117391456af47
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-05-01 11:29:55 +0600

    fix some linting issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
+// app/layout.tsx
 'use client';
 
-import "./globals.css";
-import { NavBar } from "@/components/navBar/navbar";
-import StoreProvider from "@/app/utils/provider/StoreProvider";
-import { useEffect } from "react";
-import { useAppDispatch } from "@/configs/redux/hooks";
-import { fetchLoggedInUser } from "@/configs/redux/auth/authSlice";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-
+import './globals.css';
+import { NavBar } from '@/components/navBar/navbar';
+import StoreProvider from '@/app/utils/provider/StoreProvider';
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/configs/redux/hooks';
+import { fetchLoggedInUser } from '@/configs/redux/auth/authSlice';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ReduxInitializer = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useAppDispatch();
@@ -19,25 +18,18 @@ const ReduxInitializer = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
 
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <StoreProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body>
-          <ReduxInitializer>
-            <NavBar />
-            <ToastContainer />
-            <div className="mt-16">
-              {children}
-            </div>
-          </ReduxInitializer>
-        </body>
-      </html>
+      <ReduxInitializer>
+        <NavBar />
+        <ToastContainer />
+        <div className="mt-16">{children}</div>
+      </ReduxInitializer>
     </StoreProvider>
   );
 }
