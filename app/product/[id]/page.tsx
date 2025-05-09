0654473720255db1013c83a2452989e014c98184
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-05-09 09:22:10 +0600

    fixed a terriable error

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { ProductDetails } from "@/components/product/productDetails"
 
-export default async function Page({ params }: { params: { id: string } }) {
+
+export default async function Page({ params }) {
   const { id } = await params
+  
   return (
     <div className="mt-[7.5rem] bg-gray-100">
       <ProductDetails productId={id} />
     </div>
-  )
+  );
 }
