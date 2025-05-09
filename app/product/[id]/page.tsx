e3c8a709a6e8be518bf7c5a220729be5acb8db9d
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-05-09 08:58:41 +0600

    removed mixedup contents on the page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,17 +1,10 @@
 import { ProductDetails } from "@/components/product/productDetails"
 
-<<<<<<< HEAD
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = await params
   return (
     <div className="mt-[7.5rem] bg-gray-100">
       <ProductDetails productId={id} />
-=======
-export default function Page({params}: { params: {id: string}}) {
-  return (
-    <div className="mt-[7.5rem] bg-gray-100">
-      <ProductDetails productId = {params.id}/>
->>>>>>> 981153660716c2c1c00e35f04726c4adafe20e94
     </div>
   )
 }
